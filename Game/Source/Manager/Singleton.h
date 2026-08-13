7152b04a2e6639f9d257ded40c22ea33ac90c492
BOOK-P5J347BQL9\user <runic1541@gmail.com>
2026-08-13 11:20:04 +0900

    Build System #1 : Add Singleton Template

---
+++ Game/Source/Manager/Singleton.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+template<typename T>
+class Singleton
+{
+public:
+
+	static T& Get()
+	{
+		static T instance;
+		return instance;
+	}
+
+	// 복사 생성자, 이동 생성자 삭제
+	Singleton(const Singleton&) = delete;
+	Singleton(Singleton&&) = delete;
+
+	// 대입, 복사 연산 삭제
+	Singleton& operator=(const Singleton&) = delete;
+	Singleton& operator=(Singleton&&) = delete;
+
+	virtual void Initialize() = 0;
+	virtual void Shutdown() = 0;
+
+protected:
+	Singleton() = default;
+	virtual ~Singleton() = default;
+
+};
